6d2b37ec2da999145701e6dca184c1a87c5edab3
Nashira Emery <nashiraemery1@gmail.com>
2017-11-09 19:10:35 -0500

    sinatra job posting

---
+++ hwsinatrajob.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'httparty'
+require 'nokogiri'
+
+
+
+get '/' do
+
+  url = "https://miami.craigslist.org/search/sof"
+  response = HTTParty.get url
+  html = response.body
+  dom = Nokogiri::HTML(html)
+  links = dom.css("a.hdrlnk")
+
+  links.map do |link|
+      '<h3>' + link.to_str + " " + link['href'] + '<h3>'
+    end
+end
