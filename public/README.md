4359fb1e6fbdfa7e9ea2662c4a177d7fb2b36ffc
Nashira Emery <nashiraemery1@gmail.com>
2018-03-03 22:16:57 -0500

    commit new and modified files

---
+++ public/README.md
@@ -0,0 +1,12 @@
+# Sinatra_param.server
+
+-Using query string parameters to request the jobs list for any type of job on Craigslist.
+-Parameter is a hash that communicates with server & browser, and browser & server
+
+=======
+
+Technology used: Ruby 2.4.2, Sinatra Framework, Gems: sinatra, httparty, and nokogiri
+
+=======
+
+Deployemnt: in terminal: ruby sever.rb and visit localhost:4567/?job=sof or http://localhost:4567/?job=sss to display the list of software jobs.
--- server.rb
+++ server.rb
@@ -3,29 +3,6 @@ require 'httparty'
 require 'nokogiri'
 
 
-
-# get '/' do
-#
-#
-# url = "https://miami.craigslist.org/search/sof"
-# response = HTTParty.get url
-# html = response.body
-# dom = Nokogiri::HTML(html)
-# links = dom.css("a.hdrlnk")
-#
-# links.map do |link|
-#     '<h3>' + link.to_str + " " + link['href'] + '<h3>'
-#   end
-# end
-
-
-
-
-
-
-# part 2:Make it possible to use query string parameters to request the jobs list for any type of job on Craigslist. For example, if I visit localhost:4567/?job=sof your server should display the list of software jobs.
-
-# params {}
 get '/' do
 
 
